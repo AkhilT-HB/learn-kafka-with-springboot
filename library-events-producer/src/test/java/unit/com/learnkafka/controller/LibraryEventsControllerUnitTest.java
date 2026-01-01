35050a8fa6c547f72fa023ec588cf311011d8fd9
AkhilT-HB <akhilhb02@gmail.com>
2026-01-01 12:03:17 +0530

    Implemented exception handling using controller advice

--- library-events-producer/src/test/java/unit/com/learnkafka/controller/LibraryEventsControllerUnitTest.java
+++ library-events-producer/src/test/java/unit/com/learnkafka/controller/LibraryEventsControllerUnitTest.java
@@ -17,6 +17,7 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.isA;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 //Test Slice
@@ -42,6 +43,24 @@ class LibraryEventsControllerUnitTest {
                 .content(json)
                 .contentType(MediaType.APPLICATION_JSON)
         ).andExpect(status().isCreated());
+        //then
+    }
+
+    @Test
+    void postLibraryEvent_4xx() throws Exception {
+        //given
+        var json = objectMapper.writeValueAsString(TestUtil.libraryEventRecordWithInvalidBook());
+        when(libraryEventsProducer.sendLibraryEvent_approach3(isA(LibraryEvent.class)))
+                .thenReturn(null);
+
+        var expectedErrorMessage = "book.bookId - must not be null, book.bookName - must not be blank ";
+
+        //when
+        mockMvc.perform(MockMvcRequestBuilders.post("/v1/libraryevent")
+                .content(json)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().is4xxClientError())
+                .andExpect(content().string(expectedErrorMessage));
         //then
     }
 }
