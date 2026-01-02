79962a42d9975fc0ef820ff2d5205589b1a47964
AkhilT-HB <akhilhb02@gmail.com>
2026-01-03 00:54:56 +0530

    Implemented Configuration class and consumer method

---
+++ library-events-consumer/src/main/java/com/learkafka/config/LibraryEventsConsumerConfig.java
@@ -0,0 +1,9 @@
+package com.learkafka.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+
+@Configuration
+//@EnableKafka
+public class LibraryEventsConsumerConfig {
+}
